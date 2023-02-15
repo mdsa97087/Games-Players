4fe6f9dddbfd0b1cb95168b9ff68173fbb6b93a5
mdsa97087 <mdsa97087@gmail.com>
2023-02-15 23:43:41 +0530

    pagfe

--- Index_Pages/index.js
+++ Index_Pages/index.js
@@ -1,7 +1,10 @@
 GetData();
 async function GetData() {
+  let search = document.getElementById("search").value;
   try {
-    let res = await fetch(`https://www.balldontlie.io/api/v1/players?${limit=10}`);
+    let res = await fetch(
+      `https://www.balldontlie.io/api/v1/players?search=${search}&per_page=${10}`
+    );
     let data = await res.json();
     data = data.data;
     console.log(data);
@@ -17,20 +20,20 @@ function ShowData(data) {
   data.forEach((el, i) => {
     let div = document.createElement("div");
     div.setAttribute("class", "middleDiv");
-       let imgSrc =
+    let imgSrc =
       "https://images.pexels.com/photos/9002742/pexels-photo-9002742.jpeg?auto=compress&cs=tinysrgb&w=600";
     let img = document.createElement("img");
     img.src = `${imgSrc}`;
-    img.setAttribute('class','img')
+    img.setAttribute("class", "img");
     let name = document.createElement("h3");
-    name.innerText =`Name:- ${el.first_name}`;
+    name.innerText = `Name:- ${el.first_name}`;
     let position = document.createElement("p");
-    position.innerText =`Position:-  ${el.position}`;
-    let btn=document.createElement('button')
-    btn.innerText='TEAM DETAILS'
-    btn.setAttribute('class','btn')
-    div.append(img,name, position,btn );
+    position.innerText = `Position:-  ${el.position}`;
+    let btn = document.createElement("button");
+    btn.innerText = "TEAM DETAILS";
+    btn.setAttribute("class", "btn");
+    div.append(img, name, position, btn);
     container.append(div);
   });
 }
-ShowData();
+// ShowData();
